742e3e63c40817415192afc0bc0ec1f8e2b625ac
TimRos <tim.rosezin@gmail.com>
2016-11-19 11:37:59 +0100

    Created Basic View, Layout and Model

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html', { 'treasures': treasures })
+
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+
+treasures = [
+    Treasure("Gold Nugget", 500.00, "Gold", "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, "pyrite", "Fool's Falls, CO"),
+    Treasure("Coffee Can", 20.00, "tin", "Acme, CA")
+]
